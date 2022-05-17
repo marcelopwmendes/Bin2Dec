9437d18c71de47ec9964f401eb881c1f8748db12
Marcelo Mendes <marcelopwmendes@gmail.com>
2022-05-18 00:09:51 +0100

    Added some blue to the theme

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import Input from './components/Input';
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1> Binary to Decimal Converter </h1>
-      </div>
+      <nav className="flex items-center justify-between flex-wrap bg-cyan-900 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <span class="font-semibold text-xl tracking-tight">Bin2Dec</span>
+        </div>
+        <div class="block md:hidden">
+          <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          </button>
+        </div>
+        
+      </nav>
+      <span className='bg-cyan-600 h-20 flex'>
+        <h1 className='text-left my-auto mx-6 text-cyan-100 text-2xl'> A Binary to Decimal <br/> Converter </h1>
+      </span>
 
-      <Input/>
+      <Input />
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -75,31 +75,33 @@ const Input = () => {
     }
 
     return (
-        <div>
+        <div className='bg-blue-400 h-56 my-auto flex flex-col justify-center'>
             {isBinToDecConversion
                 ? <>
                     <div>
-                        {!isBinaryNumber ? <div> <label style={{border: "solid", borderColor: "#F8D7DA", backgroundColor: "#F8D7DA", color: "#721C24"}}> The inserted number is not binary! </label> </div> : ""}
-                        <label id="label_number_binary"> Enter binary Number </label>
-                        <input type="number" id="number_binary" value={binNum} onChange={handleBinaryInput} />
+                        {!isBinaryNumber ? <div> <label style={{ border: "solid", borderColor: "#F8D7DA", backgroundColor: "#F8D7DA", color: "#721C24" }}> The inserted number is not binary! </label> </div> : ""}
+                        <label id="label_number_binary" className='flex text-gray-700 text-sm font-bold my-auto mx-6'> Enter binary Number </label>
+                        <input type="number" id="number_binary" value={binNum} className="block w-100 h-10 px-3 my-auto mx-6 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" onChange={handleBinaryInput} />
                     </div><div>
-                        <label id="label_number_binary"> Decimal number </label>
-                        <input type="number" id="number_decimal" readOnly={true} value={decNum} />
+                        <label id="label_number_binary" className='flex text-gray-700 text-sm font-bold my-auto mx-6'> Decimal number </label>
+                        <input type="number" id="number_decimal" readOnly={true} value={decNum} className="block w-100 h-10 px-3 my-auto mx-6 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" />
                     </div>
                 </>
                 : <>
                     <div>
-                        <label id="label_number_binary"> Enter decimal number </label>
-                        <input type="number" id="number_decimal" value={decNum} onChange={handleDecimalInput} />
+                        <label id="label_number_binary" className='flex text-gray-700 text-sm font-bold my-auto mx-6'> Enter decimal number </label>
+                        <input type="number" id="number_decimal" value={decNum} className="block w-100 h-10 px-3 my-auto mx-6 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" onChange={handleDecimalInput} />
                     </div><div>
-                        <label id="label_number_binary"> Binary Number </label>
-                        <input type="number" id="number_binary" readOnly={true} value={binNum} />
+                        <label id="label_number_binary" className='flex text-gray-700 text-sm font-bold my-auto mx-6'> Binary Number </label>
+                        <input type="number" id="number_binary" readOnly={true} value={binNum} className="block w-100 h-10 px-3 my-auto mx-6 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" />
                     </div>
                 </>
             }
 
-            <button id="button_convert_to_decimal" onClick={handleClick}> Convert </button>
-            <button id="button_swap_conversion" onClick={handleSwapConversion}> Swap </button>
+            <span className='flex flex-row my-4 mx-6 w-auto'>
+                <button id="button_convert_to_decimal" className='w-24 mr-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={handleClick}> Convert </button>
+                <button id="button_swap_conversion" className='w-24 text-blue-500 hover:text-blue-700 bg-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={handleSwapConversion}> Swap </button>
+            </span>
         </div>
     );
 }
